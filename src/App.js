9de74f564a4bdcecff3da1a89dc764776daa4e08
Bianca Velázquez <biancavlz@users.noreply.github.com>
2026-02-03 10:47:00 +0100

    Add conditional rendering to stats component

--- src/App.js
+++ src/App.js
@@ -106,6 +106,14 @@ function PackingList({ items, onDeleteItem, handlePackedToggle }) {
 }
 
 function Stats({ items }) {
+  if (!items.length) {
+    return (
+      <footer className="stats">
+        <em>Start adding some items to your packin list! 🚀</em>
+      </footer>
+    );
+  }
+
   const numItems = items.length;
   const numPacked = items.filter((item) => item.packed).length;
   const percentage = Math.round((numPacked / numItems) * 100);
