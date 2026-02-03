0bb50552d63141dbeabf6c418131176c42508a25
Bianca Velázquez <biancavlz@users.noreply.github.com>
2026-02-03 11:36:05 +0100

    Add confirm window to avoid clearing list by mistake

--- src/App.js
+++ src/App.js
@@ -20,7 +20,11 @@ export default function App() {
   }
 
   function handleClearList() {
-    setItems([]);
+    const confirmed = window.confirm(
+      "Are you sure you want to clear the list?",
+    );
+
+    if (confirmed) setItems([]);
   }
 
   return (
